1c7097e6a91c0152eae780483da65f22b051472b
AndyStar <andy_star@ukr.net>
2025-05-03 13:14:48 +0300

    Redundant field removed

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -93,7 +93,7 @@ class MovieDetailSerializer(MovieSerializer):
 class MovieSessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = MovieSession
-        fields = ("id", "show_time", "movie", "cinema_hall", "movie_image")
+        fields = ("id", "show_time", "movie", "cinema_hall",)
 
 
 class MovieSessionListSerializer(MovieSessionSerializer):
