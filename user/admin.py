c3caf718f07be872409f6cf9834484922573c384
AndyStar <andy_star@ukr.net>
2025-05-06 12:01:02 +0300

    formatting amended

--- user/admin.py
+++ user/admin.py
@@ -9,11 +9,25 @@ class UserAdmin(DjangoUserAdmin):
     """Define admin model for custom User model with no email field."""
 
     fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name")}),
-        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser",
-                                       "groups", "user_permissions")}),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+        (None,
+            {"fields":
+                ("email",
+                 "password")}),
+        (_("Personal info"),
+            {"fields":
+                ("first_name",
+                 "last_name")}),
+        (_("Permissions"),
+            {"fields":
+                ("is_active",
+                 "is_staff",
+                 "is_superuser",
+                 "groups",
+                 "user_permissions")}),
+        (_("Important dates"),
+            {"fields":
+                ("last_login",
+                 "date_joined")}),
     )
     add_fieldsets = (
         (None, {
